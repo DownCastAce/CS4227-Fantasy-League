5b5bc72a24c650d6c2ec46e92f6645112970697f
Tom Nolan <Tom Nolan>
2015-11-16 13:57:46 +0000

    Edited Context Object

--- FantaysLeague/src/main/java/interceptor/ServerReplyContext.java
+++ FantaysLeague/src/main/java/interceptor/ServerReplyContext.java
@@ -1,5 +1,16 @@
 package interceptor;
 
-public class ServerReplyContext {
+import java.util.Date;
 
+public class ServerReplyContext {
+	
+	private Date startTime;
+	
+	public ServerReplyContext(Date startTime){
+		this.startTime = startTime;
+	}
+	
+	public Date getStartTime(){
+		return startTime;
+	}
 }
