0aaf4a94533a6caa714cf7208162073a323e4036
Tom Nolan <Tom Nolan>
2015-11-16 13:55:44 +0000

    Updated Controllers

--- FantaysLeague/src/main/java/controllers/GoToLeagueViewCommand.java
+++ FantaysLeague/src/main/java/controllers/GoToLeagueViewCommand.java
@@ -1,13 +1,21 @@
 package controllers;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.util.HashMap;
+import java.util.Map;
+
 import models.League;
 import models.User;
+import stats.CareTaker;
 import views.SoccerLeagueView;
 
 public class GoToLeagueViewCommand implements Command{
 	
 	private League league;
 	private User user;
+	private CareTaker caretaker;
 	
 	public GoToLeagueViewCommand(League league, User user){
 		this.league = league;
@@ -18,7 +26,34 @@ public class GoToLeagueViewCommand implements Command{
 	public void execute() {
 		// TODO Auto-generated method stub
 		SoccerLeagueView view = new SoccerLeagueView();
-		LeagueController control = new LeagueController(league, view,user);
+		CareTaker caretaker = loadCareTaker();
+		LeagueController control = new LeagueController(league,view,user,caretaker);
+	}
+	
+	private static CareTaker loadCareTaker() {
+
+		CareTaker caretaker = null;
+		try {
+			FileInputStream fileIn = new FileInputStream("resources/stats/caretaker.ser");
+			ObjectInputStream in = new ObjectInputStream(fileIn);
+			caretaker = (CareTaker) in.readObject();
+			
+			in.close();
+			fileIn.close();
+			
+			return caretaker;
+			
+		} catch (IOException i) 
+		{
+			return caretaker = new CareTaker();
+			
+		}
+		catch (ClassNotFoundException c) 
+		{
+			c.printStackTrace();
+		}
+		
+		return null;
 	}
 
 }
--- FantaysLeague/src/main/java/controllers/LeagueController.java
+++ FantaysLeague/src/main/java/controllers/LeagueController.java
@@ -2,34 +2,74 @@ package controllers;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.util.HashMap;
+import java.util.Map;
 
 import models.League;
+import models.SoccerTeam;
 import models.User;
+import stats.CareTaker;
+import stats.StatMomento;
 import views.SoccerLeagueView;
 
 public class LeagueController {
-	
+
 	private League league;
 	private SoccerLeagueView view;
 	private User user;
-	
-	public LeagueController(League league, SoccerLeagueView view, User user){
+	private CareTaker caretaker;
+
+	public LeagueController(League league, SoccerLeagueView view, User user, CareTaker caretaker) {
 		this.league = league;
 		this.view = view;
 		this.user = user;
-		
+		this.caretaker = caretaker;
 		view.addReturnListener(new ListenForReturn());
+		Map<String, Integer> map = new HashMap<String,Integer>();
+		map = this.caretaker.get(0).getState();
 		
+		for(Map.Entry<String, Integer> e : map.entrySet())
+			System.out.println(e.getKey());
 		view.setVisible(true);
-		
+
 		view.updateLeagueTable(league.getLeagueTeams());
 	}
 
-	class ListenForReturn implements ActionListener{
+	private void saveState() {
+
+		if(league.saveToMomento() != null)
+		{
+			caretaker.add(league.saveToMomento());
+			saveCareTaker();
+			league.setUpdated(false);
+		}
+	}
+
+	private void saveCareTaker() {
+		try {
+			FileOutputStream fileOut = new FileOutputStream("resources/stats/caretaker.ser");
+			ObjectOutputStream out = new ObjectOutputStream(fileOut);
+			out.writeObject(caretaker);
+			out.close();
+			fileOut.close();
+			System.out.println("Saved New CareTaker");
+
+		} catch (IOException i) {
+			i.printStackTrace();
+		}
+	}
+
+	class ListenForReturn implements ActionListener {
 		@Override
 		public void actionPerformed(ActionEvent arg0) {
 			// TODO Auto-generated method stub
+			saveState();
+
 			GoBackToMainMenuCommand command = new GoBackToMainMenuCommand(user);
+			saveState();
 			command.execute();
 			view.dispose();
 		}
